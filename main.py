26661bd91c558e209a468030ceb266075e14d5c9
Aravindh NC <76737168+NC-Aravindh@users.noreply.github.com>
2024-09-18 18:48:01 +0530

    Added comments to main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ screen = Screen()
 screen.tracer(0)
 screen.setup(width=600,height=600)
 
+#Controls of the game
 screen.listen()
 screen.onkey(snake.move_up,"Up")
 screen.onkey(snake.move_down,"Down")
@@ -23,7 +24,7 @@ while game_on:
     screen.update()
     time.sleep(0.1)
 
-
+    #When snake eats the food, score updates and snake grows larger.
     snake.move_forward()
     if snake.head.distance(food) < 15:
         score.update_score()
@@ -31,11 +32,12 @@ while game_on:
         snake.extend()
 
 
-
+   #Snake touches the boundaries of the screen, its game over.
     if snake.head.xcor() > 290 or snake.head.ycor() > 290 or snake.tail.xcor() < -290 or snake.tail.ycor() <- 290 :
         game_on = False
         score.game_over()
 
+    #Snake head touches any of its body parts , its game over.
     for segment in snake.segments[1:]:
 
         if snake.head.distance(snake.tail) < 10:
@@ -43,4 +45,5 @@ while game_on:
             score.game_over()
 
 print("Game over")
+#Game Window stays open
 screen.mainloop()
